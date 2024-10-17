9301e9606bd4c46268213159c00915d0a6362f59
valmikGit <quantumcomputingiiitb@gmail.com>
2024-10-18 01:26:15 +0530

    made a user model.

--- plantbackend/plantapi/models.py
+++ plantbackend/plantapi/models.py
@@ -1,3 +1,53 @@
+from collections.abc import Iterable
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+class CustomAccountManager(BaseUserManager):
+    def create_superuser(self, email, username, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
 
-# Create your models here.
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(email, username, password, **other_fields)
+    def create_user(self, email, username, password, **other_fields):
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+        
+        other_fields.setdefault('is_active', True)
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **other_fields)
+        # password = make_password(password=password)
+        user.set_password(password)
+        user.save()
+        return user
+    
+class User(AbstractUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    # is_Email_Verified = models.BooleanField(default=False)
+    # is_Phone_Verified = models.BooleanField(default=False)
+    # otp = models.CharField(max_length=6, null=True, blank=True)
+    id = models.AutoField(primary_key=True)
+
+    objects = CustomAccountManager()
+
+    # USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def get_absolute_url(self):
+        return reverse("users:detail", kwargs={"username": self.username})
+    
+class History(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    prediction = models.TextField()
+    image = models.ImageField()
+    user_comments = models.TextField()
--- plantbackend/plantbackend/settings.py
+++ plantbackend/plantbackend/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'plantapi',
-    'rest_framework'
+    'rest_framework',
+    'plantapi'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'plantapi.User'
