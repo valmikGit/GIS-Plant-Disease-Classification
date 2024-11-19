893301525c54894e287bd4ac39d474e49a291c77
Vaibhav <Vaibhav.Mittal@iiitb.ac.in>
2024-11-20 00:15:10 +0530

    made upload image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Seedling from "./pages/Seedling";
+import { ImageProvider } from "./context/Base64Decode";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/seedling" element={<Seedling />} />
-      </Routes>
-    </BrowserRouter>
+    <ImageProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/seedling" element={<Seedling />} />
+        </Routes>
+      </BrowserRouter>
+    </ImageProvider>
   );
 }
 
